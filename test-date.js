92b0a7dec31706a3044fa5cf1c10107c1a9c0ce8
rama chandran <vjysolutions@gmail.com>
2026-03-25 07:30:50 +0530

    check

--- test-date.js
+++ test-date.js
@@ -4,3 +4,4 @@ console.log('Original', prevDate);
 prevDate.setUTCDate(prevDate.getUTCDate() - 1);
 const prevDateStr = prevDate.toISOString().split('T')[0];
 console.log('Result', prevDateStr);
+console.log(new Date('2026-03-14T00:00:00Z'));
